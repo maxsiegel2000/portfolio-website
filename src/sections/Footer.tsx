4037571452451ac089b2747d7dbf7d8c50d0909c
maxsiegel2000 <siegelmax026@gmail.com>
2024-12-13 11:49:50 +0100

    changing eslintrc

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 dark:radial_mask_dark radial_mask_light -z-10"></div>
       <div className="container relative z-10">
         <div className="border-t dark:border-white/15 border-black/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
-          <div className="dark:text-white/40 text-black/40">@ 2024. All rights reserved.</div>
+          <div className="dark:text-white/40 text-black/40">&copy; 2024. All rights reserved.</div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map(link => (
              <a href={link.href} key={link.title} className="inline-flex items-center gap-1.5 hover:underline">
